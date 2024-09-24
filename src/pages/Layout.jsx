2dabbbefb111359ed37bbfa6e7744dd5490e7a65
Saidazizhon <asrarovs26@gmail.com>
2024-09-24 20:09:26 +0500

    tugadi

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,29 @@
+import { Link, Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-blue-900 text-white">
+        <div className="p-4 text-center text-2xl font-bold">Logo</div>
+        <nav>
+          <ul>
+            <li className="p-4 hover:bg-blue-700">
+              <Link to="/users">Users</Link>
+            </li>
+            <li className="p-4 hover:bg-blue-700">
+              <Link to="/companies">Companies</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+
+      {/* Main content */}
+      <div className="flex-1 bg-gray-100 p-6">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
